1dd9a21dac89ba9be0b0b0745bce0bab390afdae
davronnnn <kifleader@gmail.com>
2022-04-15 18:59:10 +0500

    api

--- src/sections/Form/index.jsx
+++ src/sections/Form/index.jsx
@@ -9,7 +9,7 @@ const FormSection = ({ lang, text }) => {
 	const formHandler = () => {
 		axios
 			.post(
-				`http://megacom.cdialogues.com/api/v1.0/X_VICTORINA/NOTIFY?MSISDN=${phone}&LOGIN=VALADDSERV&PASSWORD=Qa5BA3jA`
+				`https://megacom.cdialogues.com/api/v1.0/X_VICTORINA/NOTIFY?MSISDN=${phone}&LOGIN=VALADDSERV&PASSWORD=Qa5BA3jA`
 			)
 			.then((res) => {
 				console.log(res);
@@ -21,43 +21,45 @@ const FormSection = ({ lang, text }) => {
 			});
 	};
 	return (
-		<Layout>
-			<div className='message flex mtb'>
-				<div className='coin-title '>
-					<h1>
-						{lang === 'uz' ? text[19]?.uz_text : ''}
-						{lang === 'ru' ? text[19]?.ru_text : ''}
-						{lang === 'en' ? text[19]?.en_text : ''}
-						{lang === 'kz' ? text[19]?.kyr_text : ''}
-					</h1>
-					<div className='submits'>
-						<input
-							type='text'
-							placeholder='996ХХХХХХХХХ '
-							value={phone}
-							onChange={(e) => {
-								setPhone(e.target.value);
-							}}
-						/>
-						<button
-							onClick={formHandler}
-							style={
-								isSuccess
-									? {
-											color: 'green',
-											border: ' 1px solid green',
-									  }
-									: { color: '#58227a' }
-							}>
-							{isSuccess ? 'Успешно' : 'Отправить'}
-						</button>
+		<section className='form-section'>
+			<Layout>
+				<div className='message flex mtb'>
+					<div className='coin-title '>
+						<h1>
+							{lang === 'uz' ? text[19]?.uz_text : ''}
+							{lang === 'ru' ? text[19]?.ru_text : ''}
+							{lang === 'en' ? text[19]?.en_text : ''}
+							{lang === 'kz' ? text[19]?.kyr_text : ''}
+						</h1>
+						<div className='submits'>
+							<input
+								type='text'
+								placeholder='996ХХХХХХХХХ '
+								value={phone}
+								onChange={(e) => {
+									setPhone(e.target.value);
+								}}
+							/>
+							<button
+								onClick={formHandler}
+								style={
+									isSuccess
+										? {
+												color: 'green',
+												border: ' 1px solid green',
+										  }
+										: { color: '#58227a' }
+								}>
+								{isSuccess ? 'Успешно' : 'Отправить'}
+							</button>
+						</div>
+					</div>
+					<div className='coin-image'>
+						<img src={messageImg} alt='coins' />
 					</div>
 				</div>
-				<div className='coin-image'>
-					<img src={messageImg} alt='coins' />
-				</div>
-			</div>
-		</Layout>
+			</Layout>
+		</section>
 	);
 };
 
