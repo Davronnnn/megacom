63fbc0bc93c681ef16cc65fc19f95bacc6454140
davronnnn <kifleader@gmail.com>
2022-05-24 13:11:13 +0500

    applinks

--- src/sections/Form/index.jsx
+++ src/sections/Form/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import './form.scss';
-import messageImg from '../../assets/message-coins.png';
 import axios from 'axios';
+
+import appstoreIcon from '../../assets/appstore.png';
+import messageImg from '../../assets/message-coins.png';
 import Layout from '../../components/Layout';
+
+import './form.scss';
 const FormSection = ({ text, lang }) => {
 	const [phone, setPhone] = useState('');
 	const [message, setMessage] = useState(
@@ -18,6 +21,17 @@ const FormSection = ({ text, lang }) => {
 	const [isSuccess, setIsSuccess] = useState(false);
 	const [isError, setIsError] = useState(false);
 
+	let appText, appStoreLink, playMarketLink;
+	console.log(text[48].name, text[47].name);
+	if (lang == 'uz') {
+		appText = text[48]?.uz_text ? text[48]?.uz_text : 'App';
+	} else if (lang == 'ru') {
+		appText = text[48]?.ru_text ? text[48]?.ru_text : 'Приложение';
+	} else if (lang == 'en') {
+		appText = text[48]?.en_text ? text[48]?.en_text : 'App';
+	} else {
+		appText = text[48]?.kg_text ? text[48]?.kg_text : 'App';
+	}
 	useEffect(() => {
 		let time = localStorage.getItem('time');
 		let currentTime = new Date().getMinutes();
@@ -139,6 +153,44 @@ const FormSection = ({ text, lang }) => {
 									{message}
 								</button>
 							)}
+						</div>
+						<div className='application'>
+							<button className='application-button'>
+								{appText}
+							</button>
+							<div className='icons'>
+								<a href={playMarketLink}>
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										viewBox='0 0 48 48'
+										width='48px'
+										height='48px'>
+										<path
+											fill='#4db6ac'
+											d='M7.705,4.043C7.292,4.15,7,4.507,7,5.121c0,1.802,0,18.795,0,18.795S7,42.28,7,43.091c0,0.446,0.197,0.745,0.5,0.856l20.181-20.064L7.705,4.043z'
+										/>
+										<path
+											fill='#dce775'
+											d='M33.237,18.36l-8.307-4.796c0,0-15.245-8.803-16.141-9.32C8.401,4.02,8.019,3.961,7.705,4.043l19.977,19.84L33.237,18.36z'
+										/>
+										<path
+											fill='#d32f2f'
+											d='M8.417,43.802c0.532-0.308,15.284-8.825,24.865-14.357l-5.601-5.562L7.5,43.947C7.748,44.038,8.066,44.004,8.417,43.802z'
+										/>
+										<path
+											fill='#fbc02d'
+											d='M41.398,23.071c-0.796-0.429-8.1-4.676-8.1-4.676l-0.061-0.035l-5.556,5.523l5.601,5.562c4.432-2.559,7.761-4.48,8.059-4.653C42.285,24.248,42.194,23.5,41.398,23.071z'
+										/>
+									</svg>
+								</a>
+								<a href={appStoreLink}>
+									<img
+										src={appstoreIcon}
+										alt='appstore link'
+										srcset=''
+									/>
+								</a>
+							</div>
 						</div>
 					</div>
 					<div className='coin-image'>
